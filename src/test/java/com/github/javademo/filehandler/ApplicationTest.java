b645408949ec2e25c976cd76b90410e7ff4d6f1f
Dobrosi Andras <dobrosi.andras@hmei.hu>
2021-02-19 12:35:23 +0100

    test fix

--- src/test/java/com/github/javademo/filehandler/ApplicationTest.java
+++ src/test/java/com/github/javademo/filehandler/ApplicationTest.java
@@ -1,19 +1,40 @@
 package com.github.javademo.filehandler;
 
 import static com.github.javademo.filehandler.Application.main;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest {
 
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
 	@Test
 	void testMain() {
+		captureOut();
 		main(new String[] {});
+		assertTrue(getOut().startsWith("Missing required option:"));
 	}
 
 	@Test
 	void testRun() {
-		new Application().withIncomingDir("INCOMING").run();
+		await().until(() -> {
+			new Application().withIncomingDir("INCOMING").run();
+			return true;
+		});
+	}
+
+	private void captureOut() {
+		System.setErr(new PrintStream(outContent));
+	}
+
+	private String getOut() {
+		return outContent.toString().replaceAll("\r\n", "");
+
 	}
 
 }
--- src/test/java/com/github/javademo/filehandler/TestUtils.java
+++ src/test/java/com/github/javademo/filehandler/TestUtils.java
@@ -1,52 +1,46 @@
 package com.github.javademo.filehandler;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.awaitility.Awaitility.await;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
 import org.springframework.core.io.ClassPathResource;
+
 import com.github.javademo.filehandler.exception.FilehandlerRuntimeException;
 
 public class TestUtils {
 
-  public static final String FILE_FIXTURE_PATH = "/data/foo.txt";
+	public static final String FILE_FIXTURE_PATH = "/data/foo.txt";
 
-  public static final String FILE_FIXTURE_NAME = "foo.txt";
+	public static final String FILE_FIXTURE_NAME = "foo.txt";
 
-  public static File locateClasspathResource(String resourceName) {
-    try {
-      return new ClassPathResource(resourceName).getFile();
-    } catch (IOException e) {
-      throw new FilehandlerRuntimeException(e);
-    }
-  }
+	public static File locateClasspathResource(String resourceName) {
+		try {
+			return new ClassPathResource(resourceName).getFile();
+		} catch (IOException e) {
+			throw new FilehandlerRuntimeException(e);
+		}
+	}
 
-  public static void assertThatDirectoryHasFiles(File directory, int expectedFileCount)
-      throws Exception {
-    int count = 0;
-    while (directory.listFiles().length < expectedFileCount && count++ < 10) {
-      Thread.sleep(500);
-    }
-    assertThat(directory.listFiles().length, is(expectedFileCount));
-  }
+	public static void assertThatDirectoryHasFiles(File directory, int expectedFileCount) throws Exception {
+		await().until(() -> directory.listFiles().length == expectedFileCount);
+	}
 
-  public static void assertThatDirectoryIsEmpty(File directory) throws Exception {
-    int count = 0;
-    while (directory.listFiles().length > 0 && count++ < 10) {
-      Thread.sleep(500);
-    }
-    assertThat(directory.listFiles().length, is(0));
-  }
+	public static void assertThatDirectoryIsEmpty(File directory) throws Exception {
+		await().until(() -> directory.listFiles().length == 0);
+	}
 
-  public static boolean deleteRecursive(File path) throws FileNotFoundException {
-    if (!path.exists()) throw new FileNotFoundException(path.getAbsolutePath());
-    boolean ret = true;
-    if (path.isDirectory()) {
-      for (File f : path.listFiles()) {
-        ret = ret && deleteRecursive(f);
-      }
-    }
-    return ret && path.delete();
-  }
+	public static boolean deleteRecursive(File path) throws FileNotFoundException {
+		if (!path.exists())
+			throw new FileNotFoundException(path.getAbsolutePath());
+		boolean ret = true;
+		if (path.isDirectory()) {
+			for (File f : path.listFiles()) {
+				ret = ret && deleteRecursive(f);
+			}
+		}
+		return ret && path.delete();
+	}
 }
